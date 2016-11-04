120c1be44a22d83f9ff9dca6e24ba0c2c539eef5
obees <info@info.net>
2016-11-04 11:29:50 +0100

    nifi additions

--- scripts/docker_machine_nifi_kafka_cluster.sh
+++ scripts/docker_machine_nifi_kafka_cluster.sh
@@ -12,8 +12,8 @@ WORKER=2
 # ######################## #
 
 # Create the Docker hosts
-for i in $(seq 1 $MANAGER); do docker-machine create --driver virtualbox manager$i; done
-for i in $(seq 1 $WORKER); do docker-machine create --driver virtualbox worker$i; done
+### for i in $(seq 1 $MANAGER); do docker-machine create --driver virtualbox manager$i; done
+### for i in $(seq 1 $WORKER); do docker-machine create --driver virtualbox worker$i; done
 
 
 # ##################################### #
@@ -21,10 +21,12 @@ for i in $(seq 1 $WORKER); do docker-machine create --driver virtualbox worker$i
 # ##################################### #
 
 # Init the swarm
-docker-machine ssh manager1 docker swarm init --advertise-addr $(docker-machine ip manager1)
+docker-machine ssh manager1 docker swarm init --advertise-addr $(docker-machine ip manager1):2377 --listen-addr $(docker-machine ip manager1):2377
+
+# docker-machine ssh manager1 docker swarm init --advertise-addr eth0 --listen-addr eth0
 
 # Get the swarm manager join token
-WORKER_JOIN_TOKEN=$(docker-machine ssh manager1 docker swarm join-token manager -q)
+MANAGER_JOIN_TOKEN=$(docker-machine ssh manager1 docker swarm join-token manager -q)
 
 # Get the swarm worker join token
 WORKER_JOIN_TOKEN=$(docker-machine ssh manager1 docker swarm join-token worker -q)
@@ -50,12 +52,145 @@ docker-machine ssh manager1 docker network create --driver overlay --subnet 10.1
 # ################################ #
 
 # Create the zookeeper service
-docker-machine ssh manager1 docker service create --name zk --publish 2181:2181 --network kafkanet jplock/zookeeper
+### docker-machine ssh manager1 docker service create --name zk --publish 2181:2181 --network kafkanet jplock/zookeeper
 
 # Create the kafka service
-docker-machine ssh manager1 docker service create --name kafka --publish 9092:9092 --publish 7203:7203 --env KAFKA_ADVERTISED_HOST_NAME=kafka --env ZOOKEEPER_IP=zk --network kafkanet ches/kafka
+### docker-machine ssh manager1 docker service create --name kafka --publish 9092:9092 --publish 7203:7203 --env KAFKA_ADVERTISED_HOST_NAME=kafka --env ZOOKEEPER_IP=zk --network kafkanet ches/kafka
 
 # Create the nifi service
-docker-machine ssh manager1 docker service create --name nifi --publish 8080:8080 --publish 8081:8081 --network ingress --network kafkanet mkobit/nifi
+### docker-machine ssh manager1 docker service create --name nifi --publish 8080:8080 --publish 8081:8081 --network ingress --network kafkanet mkobit/nifi
+
+# ########################################## #
+# create the clustered services on the swarm #
+# ########################################## #
+
+# Create the zookeeper cluster services
+docker-machine ssh manager1 "docker service create --name zoo1 --publish 2182:2181 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=0.0.0.0:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888' --network kafkanet obees/zookeeper"
+docker-machine ssh manager1 "docker service create --name zoo2 --publish 2183:2181 --env ZOO_MY_ID=2 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=0.0.0.0:2888:3888 server.3=zoo3:2888:3888' --network kafkanet obees/zookeeper"
+docker-machine ssh manager1 "docker service create --name zoo3 --publish 2184:2181 --env ZOO_MY_ID=3 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=0.0.0.0:2888:3888' --network kafkanet obees/zookeeper"
+
+#docker-machine ssh manager1 "docker service create --name zoo1 --publish 2181:2181 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=0.0.0.0:2888:3888' --env ZOO_PORT=2181 --network kafkanet obees/zookeeper"
+
+# Create the kafka cluster  service
+docker-machine ssh manager1 "docker service create --name kafka0 --publish 9092:9092 --env KAFKA_BROKER_ID=0 --env KAFKA_PORT=9092 --env KAFKA_ADVERTISED_HOST_NAME=kafka0 --env KAFKA_ADVERTISED_PORT=9092 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet ches/kafka"
+docker-machine ssh manager1 "docker service create --name kafka1 --publish 9093:9093 --env KAFKA_BROKER_ID=1 --env KAFKA_PORT=9093 --env KAFKA_ADVERTISED_HOST_NAME=kafka1 --env KAFKA_ADVERTISED_PORT=9093 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet ches/kafka"
+docker-machine ssh manager1 "docker service create --name kafka2 --publish 9094:9094 --env KAFKA_BROKER_ID=2 --env KAFKA_PORT=9094 --env KAFKA_ADVERTISED_HOST_NAME=kafka2 --env KAFKA_ADVERTISED_PORT=9094 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet ches/kafka"
+
+# Create the nifi cluster service
+
+### docker-machine ssh manager1 docker service create --name nifi --publish 8080:8080 --publish 8081:8081 --network ingress --network kafkanet mkobit/nifi
+
+#docker-machine ssh manager1 "docker service create --name kafka0 --publish 9092:9092 --env KAFKA_BROKER_ID=0 --env KAFKA_PORT=9092 --env KAFKA_ADVERTISED_HOST_NAME=kafka0 --env KAFKA_ADVERTISED_PORT=9092 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet obees/kafka:0.5"
+#docker-machine ssh manager1 "docker service create --name kafka1 --publish 9093:9093 --env KAFKA_BROKER_ID=1 --env KAFKA_PORT=9093 --env KAFKA_ADVERTISED_HOST_NAME=kafka1 --env KAFKA_ADVERTISED_PORT=9093 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet obees/kafka:0.5"
+#docker-machine ssh manager1 "docker service create --name kafka2 --publish 9094:9094 --env KAFKA_BROKER_ID=2 --env KAFKA_PORT=9094 --env KAFKA_ADVERTISED_HOST_NAME=kafka2 --env KAFKA_ADVERTISED_PORT=9094 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet obees/kafka:0.5"
 
 
+
+#docker-machine ssh manager1 "docker service create --name kafka1 --publish 9093:9092 --env KAFKA_BROKER_ID=0 --env KAFKA_ADVERTISED_HOST_NAME=kafka1 --env KAFKA_ADVERTISED_PORT=9092 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet ches/kafka"
+#docker-machine ssh manager1 "docker service create --name kafka2 --publish 9094:9092 --env KAFKA_BROKER_ID=1 --env KAFKA_ADVERTISED_HOST_NAME=kafka2 --env KAFKA_ADVERTISED_PORT=9092 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet ches/kafka"
+#docker-machine ssh manager1 "docker service create --name kafka3 --publish 9095:9092 --env KAFKA_BROKER_ID=2 --env KAFKA_ADVERTISED_HOST_NAME=kafka3 --env KAFKA_ADVERTISED_PORT=9092 --env ZOOKEEPER_CONNECTION_STRING='zoo1:2181,zoo2:2181,zoo3:2181' --network kafkanet ches/kafka"
+
+
+# Create the nifi service
+
+
+# ################################ #
+# tests avec 31z4/zookeeper     #
+# ################################ #
+
+# Create the zookeeper service 1 for cluster
+### docker-machine ssh manager1 "docker service create --name zoo1 --publish 2184:2181 --env ZOO_PORT=2181 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2889:3889 server.3=zoo3:2890:3890' --network kafkanet 31z4/zookeeper"
+### docker-machine ssh manager1 "docker service create --name zoo2 --publish 2185:2182 --env ZOO_PORT=2182 --env ZOO_MY_ID=2 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2889:3889 server.3=zoo3:2890:3890' --network kafkanet 31z4/zookeeper"
+### docker-machine ssh manager1 "docker service create --name zoo3 --publish 2186:2183 --env ZOO_PORT=2183 --env ZOO_MY_ID=3 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2889:3889 server.3=zoo3:2890:3890' --network kafkanet 31z4/zookeeper"
+
+docker-machine ssh manager1 "docker service create --name zoo1 --publish 2182:2181 --publish 2889:2888 --publish 3889:3888 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888' --network kafkanet --network ingress 31z4/zookeeper"
+docker-machine ssh manager1 "docker service create --name zoo2 --publish 2183:2181 --publish 2890:2888 --publish 3890:3888 --env ZOO_MY_ID=2 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888' --network kafkanet --network ingress 31z4/zookeeper"
+
+### docker-machine ssh manager1 docker service create --name zoo1 --publish 2182:2181 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888' --network kafkanet --network ingress 31z4/zookeeper
+
+### zoo1:
+###         image: 31z4/zookeeper
+###         restart: always
+###         ports:
+###             - 2181
+###         environment:
+###             ZOO_MY_ID: 1
+###             ZOO_SERVERS: server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888
+
+
+# ################################ #
+# tests avec fabric8/zookeeper     #
+# ################################ #
+
+### docker-machine ssh manager1 docker service create --name zookeeper-1 --publish 2182:2181 --env SERVER_ID=1 --env MAX_SERVERS=3 --network kafkanet fabric8/zookeeper
+### docker-machine ssh manager1 docker service create --name zookeeper-2 --publish 2183:2181 --env SERVER_ID=2 --env MAX_SERVERS=3 --network kafkanet fabric8/zookeeper
+### docker-machine ssh manager1 docker service create --name zookeeper-3 --publish 2184:2181 --env SERVER_ID=3 --env MAX_SERVERS=3 --network kafkanet fabric8/zookeeper
+
+### SERVER_ID 	The id of the server
+### MAX_SERVERS 	The number of servers in the ensemble
+
+
+# ###################################### #
+# tests avec cgswong/confluent-zookeeper #
+# ###################################### #
+
+### docker-machine ssh manager1 docker service create --name zk1 --publish 2182:2181 --env zk_id=1 --env zk_server_1=zk1 --env zk_server_2=zk2 --env zk_server_3=zk3 --network kafkanet cgswong/confluent-zookeeper
+### docker-machine ssh manager1 docker service create --name zk2 --publish 2183:2181 --env zk_id=2 --env zk_server_1=zk1 --env zk_server_2=zk2 --env zk_server_3=zk3 --network kafkanet cgswong/confluent-zookeeper
+### docker-machine ssh manager1 docker service create --name zk3 --publish 2184:2181 --env zk_id=3 --env zk_server_1=zk1 --env zk_server_2=zk2 --env zk_server_3=zk3 --network kafkanet cgswong/confluent-zookeeper
+
+### docker run -d --name zk1 \
+###   -p 2181:2181 -p 2888:2888 -p 3888:3888 \
+###   -e zk_id=1 -e zk_server_1=172.17.8.101 -e zk_server_2=172.17.8.102 -e zk_server_3=172.17.8.103 \
+###   cgswong/confluent-zookeeper
+
+
+docker-machine ssh manager1 "docker service create --name zoo1 --publish 2182:2181 --publish 2889:2888 --publish 3889:3888 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=zoo1:2889:3889 server.2=zoo2:2890:3890' --network kafkanet --network ingress obees/zookeeper"
+docker-machine ssh manager1 "docker service create --name zoo2 --publish 2183:2181 --publish 2890:2888 --publish 3890:3888 --env ZOO_MY_ID=2 --env ZOO_SERVERS='server.1=zoo1:2889:3889 server.2=zoo2:2890:3890' --network kafkanet --network ingress obees/zookeeper"
+
+# L'accès par la virtual ip (vip) du service redirige à travers un load-balancer vers les conteneurs, 
+# les ports n'ont pas besoin d'être exposés si le service partage un network overlay avec le conteneur appelant 
+
+docker-machine ssh manager1 "docker service create --name zoo1 --publish 2181:2181 --env ZOO_MY_ID=1 --network kafkanet --network ingress 31z4/zookeeper"
+# docker-machine ssh manager1 "docker service create --name zoo2 --publish 2183:2181 --env ZOO_MY_ID=2 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888' --network kafkanet --network ingress obees/zookeeper"
+
+docker-machine ssh manager1 "docker service create --name zoo1 --publish 2182:2181 --env ZOO_MY_ID=1 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888' --network kafkanet obees/zookeeper"
+docker-machine ssh manager1 "docker service create --name zoo2 --publish 2183:2181 --env ZOO_MY_ID=2 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888' --network kafkanet obees/zookeeper"
+docker-machine ssh manager1 "docker service create --name zoo3 --publish 2184:2181 --env ZOO_MY_ID=3 --env ZOO_SERVERS='server.1=zoo1:2888:3888 server.2=zoo2:2888:3888 server.3=zoo3:2888:3888' --network kafkanet obees/zookeeper"
+
+
+
+# premiers tests nifi cluster
+
+# docker run -i -t --rm -p 8443:443 -v ${cert_path}:/opt/certs -v $(readlink -f ./authorized-users.xml):/opt/nifi/conf/authorized-users.xml -e KEYSTORE_PATH=/opt/certs/keystore.jks -e KEYSTORE_TYPE=JKS -e KEYSTORE_PASSWORD=password -e TRUSTSTORE_PATH=/opt/certs/truststore.jks -e TRUSTSTORE_PASSWORD=password -e TRUSTSTORE_TYPE=JKS apiri/apache-nifi
+# docker run -i -t --rm -p 8080:80 -e DISABLE_SSL=true aldrin/apache-nifi
+
+# Il faut travailler sur la config cluster dans un projet à part
+
+# Config minimum d'un cluster nifi 
+
+# nifi.properties 
+nifi.web.http.host= 												# nifi1, nifi2, ... à passer en variable d'environnement
+nifi.web.http.port=80 												# à passer --env
+nifi.cluster.is.node=true 											# Mettre à true si cluster
+nifi.cluster.node.address=											# Set this to the fully qualified hostname of the node. If left blank, it defaults to "localhost".
+nifi.cluster.node.protocol.port= 									# Set this to an open port that is higher than 1024 (anything lower requires root).
+nifi.cluster.node.protocol.threads=									# The number of threads that should be used to communicate with other nodes in the cluster. This property defaults to 10, but for large clusters, this value may need to be larger.
+nifi.zookeeper.connect.string=zoo1:2181,zoo2:2181,zoo3:2181			# The Connect String that is needed to connect to Apache ZooKeeper. This is a comma-separted list of hostname:port pairs. For example, localhost:2181,localhost:2182,localhost:2183. This should contain a list of all ZooKeeper instances in the ZooKeeper quorum.
+nifi.zookeeper.root.node=											# à passer -env # /nifi/nifi1 ou /nifi/nifi2
+
+# state-management.xml
+<property name="Connect String">zoo1:2181,zoo2:2181,zoo3:2181</property> # à passer --env # myhost.mydomain:2181,host2.mydomain:5555,host3:6666
+<property name="Root Node">/nifi</property> # à passer -env # /nifi/nifi1 ou /nifi/nifi2
+
+NIFI_WEB_HTTP_HOST=nifi1
+NIFI_WEB_HTTP_PORT=80
+NIFI_CLUSTER_IS_NODE=true
+NIFI_CLUSTER_NODE_ADDRESS=nifi1
+NIFI_CLUSTER_NODE_PROTOCOL_PORT=2190 
+NIFI_CLUSTER_NODE_PROTOCOL_THREADS=10 
+NIFI_ZOOKEEPER_CONNECT_STRING='zoo1:2181,zoo2:2181,zoo3:2181' 
+NIFI_ZOOKEEPER_ROOT_NODE=/nifi
+
+env NIFI_WEB_HTTP_HOST=nifi1 NIFI_WEB_HTTP_PORT=80 NIFI_CLUSTER_IS_NODE=true NIFI_CLUSTER_NODE_ADDRESS=nifi1 NIFI_CLUSTER_NODE_PROTOCOL_PORT=2190 NIFI_CLUSTER_NODE_PROTOCOL_THREADS=10 NIFI_ZOOKEEPER_CONNECT_STRING='zoo1:2181,zoo2:2181,zoo3:2181' NIFI_ZOOKEEPER_ROOT_NODE='\/nifi' ./test.sh
+
+env NIFI_WEB_HTTP_HOST=nifi1 NIFI_WEB_HTTP_PORT=80 NIFI_CLUSTER_IS_NODE=true ./test.sh
